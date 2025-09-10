cf2ed0316149b9f5be849ec025e6ef2a0267d9b7
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-10 14:33:11 -0300

    Correto

---
+++ listaExercicios.php
@@ -0,0 +1,116 @@
+<?php 
+//ex001
+$num1 = 30;
+$num2 = 10;
+$resultado = 0;
+
+function SomarouSubtarir ($num1, $num2, $resultado){
+	$resultado = $num1 + $num2;
+	if ($resultado >= 20) {
+		$resultado += 8;
+		return($resultado);
+
+	}
+	else{
+		$resultado -= 5;
+		return($resultado);
+
+	}
+
+
+}
+
+
+echo(SomarouSubtarir($num1, $num2, $resultado));
+
+//ex002
+
+
+// $numero = 2;
+
+// function informarDivisor($numero){
+// 	if($numero % 2 == 0){
+// 		echo("Numero divisivel por 2");
+
+
+
+// 	}
+// 	elseif($numero % 5 == 0){
+// 		echo("Numero divisivel por 5");
+// 	}
+
+// 	elseif($numero % 10 == 0){
+// 		echo("Numero divisivel por 10");
+// 	}
+
+// 	else{
+// 		echo("Numero não é divisive por 2, 5 ou 10");
+// 	}
+
+// }
+
+// informarDivisor($numero);
+
+
+
+//ex003
+
+// $nome = "João Pedro";
+// $idade = 17;
+// $sexo = "FEMININO";
+
+// $sexo = strtolower($sexo); // usei pois estou aprendendo isso em JAVA. O comando eu pesquisei qual era.
+
+// function Verificar($nome, $idade, $sexo){
+// 	if($sexo == "feminino" && $idade < 25){
+// 		echo("$nome <br/> ACEITA");
+
+// 	}
+// 	else{
+// 		echo("$nome <br/> NÃO ACEITA!");
+// 	}
+
+// }
+// Verificar($nome, $idade, $sexo);
+
+
+//ex004
+
+// $num1 = 1;
+// $num2 = 3;
+// $num3 = 2;
+// $temp = 0;
+// $cont = 0;
+
+// function Organizar($num1, $num2, $num3, $temp, $cont){
+// 	while ($cont < 3) {
+// 		if($num1 < $num2) {
+// 			$temp = $num2;
+// 			$num2 = $num1;
+// 			$num1 = $temp;
+// 		}
+// 		elseif($num2 < $num3){
+// 			$temp = $num3;
+// 			$num3 = $num2;
+// 			$num2 = $num1;
+// 			$num1 = $temp;
+
+
+// 		}
+	
+// 		$cont++;
+
+// 	}
+	
+// 	echo("$num1, $num2, $num3");
+
+// }
+// Organizar($num1, $num2, $num3, $temp, $cont);
+
+
+//ex005
+
+
+
+ ?>
+
