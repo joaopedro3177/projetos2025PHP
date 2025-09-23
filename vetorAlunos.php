edbf37c7ebdbbe82618c6b33baf58f67dbf1288a
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-23 20:12:03 -0300

    Initial commit

---
+++ vetorAlunos.php
@@ -0,0 +1,46 @@
+<?php 
+	$alunos = array();
+
+
+	$alunos[0]= "Maria";
+	$alunos[1]= "José";
+
+
+
+	echo($alunos[0] . "<br/>");
+	echo($alunos[1]);
+	echo("<br/>");
+	echo "<pre>";
+
+
+	$email[] = "joaogameplaycarros@gmail.com";
+	$email[] = "pindamonhangaba@gmail.com";
+	$email[] = "lazarobarboza444@gmail.com";
+
+
+	print_r($email);
+	echo("<br/>");
+	echo "<pre>";
+	var_dump($email);
+	echo "<pre>";
+
+
+
+	$aluno = array("Oslvado", 26, "osvaldo@hotmal.com",25.5, true);
+	echo ("<pre>");
+	var_dump($aluno);
+	echo("</pre>");
+
+	echo("<br/>");
+	echo ("<pre>");
+	print_r($aluno);
+	echo("</pre>");
+	
+	
+
+
+
+
+
+
+ ?>
