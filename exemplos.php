8d941c466ee76acc76291943fb322858217d1399
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-23 21:30:22 -0300

    Initial commit

--- exemplos.php
+++ exemplos.php
@@ -35,12 +35,34 @@
 </html>
 
 
+<?php 
 
+echo "<br/>";
+
+
+ ?>
 
 <html>
 	<body>
 		<?php 
 			$num = array(1,2,3,4,5);
+
+			foreach ($num as $valor) {
+				echo("Valor é $valor <br/>");
+			}
+
+
+			$num[0] = "um";
+			$num[1] = "dois";
+			$num[2] = "três";
+			$num[3] = "quatro";
+			$num[4] = "cinco";
+
+
+			foreach ($num as $valor) {
+				echo("Valor é $valor <br/>");
+			}
+
 
 
 
--- vetorAlunos.php
+++ vetorAlunos.php
@@ -1,57 +1,70 @@
 <?php 
-	$alunos = array();
+	// $alunos = array();
 
 
-	$alunos[0]= "Maria";
-	$alunos[1]= "José";
-
-
-
-	echo($alunos[0] . "<br/>");
-	echo($alunos[1]);
-	echo("<br/>");
-	echo "<pre>";
-
-
-	$email[] = "joaogameplaycarros@gmail.com";
-	$email[] = "pindamonhangaba@gmail.com";
-	$email[] = "lazarobarboza444@gmail.com";
-
-
-	print_r($email);
-	echo("<br/>");
-	echo "<pre>";
-	var_dump($email);
-	echo "<pre>";
+	// $alunos[0]= "Maria";
+	// $alunos[1]= "José";
 
 
 
-	$aluno = array("Oslvado", 26, "osvaldo@hotmal.com",25.5, true);
-	echo ("<pre>");
-	var_dump($aluno);
-	echo("</pre>");
-
-	echo("<br/>");
-	echo ("<pre>");
-	print_r($aluno);
-	echo("</pre>");
+	// echo($alunos[0] . "<br/>");
+	// echo($alunos[1]);
+	// echo("<br/>");
+	// echo "<pre>";
 
 
+	// $email[] = "joaogameplaycarros@gmail.com";
+	// $email[] = "pindamonhangaba@gmail.com";
+	// $email[] = "lazarobarboza444@gmail.com";
 
-	for ($i=0; $i < 6 ; $i++) { 
-		$vetor[$i] = 12;
+
+	// print_r($email);
+	// echo("<br/>");
+	// echo "<pre>";
+	// var_dump($email);
+	// echo "<pre>";
+
+
+
+	// $aluno = array("Oslvado", 26, "osvaldo@hotmal.com",25.5, true);
+	// echo ("<pre>");
+	// var_dump($aluno);
+	// echo("</pre>");
+
+	// echo("<br/>");
+	// echo ("<pre>");
+	// print_r($aluno);
+	// echo("</pre>");
+
+
+
+	// for ($i=0; $i < 6 ; $i++) { 
+	// 	$vetor[$i] = 12;
 		
 
-	}
+	// }
+
+	// echo("<br/>");
+
+	// for ($i=0; $i < 6 ; $i++) { 
+	// 	echo "O valor do vetor: " . $vetor[$i];
+	// 	echo("<br/>");
+	// }
+	
+	
 
 	echo("<br/>");
 
-	for ($i=0; $i < 6 ; $i++) { 
-		echo "O valor do vetor: " . $vetor[$i];
-		echo("<br/>");
-	}
-	
-	
+
+	$nome["MA"] = "Maria";
+	$nome["PE"] = "Maria";
+	$nome["LALA"] = "Lazaro Barboza";
+
+	echo("<pre>");
+	var_dump($nome);
+	echo("</pre>");
+
+
 
 
 
