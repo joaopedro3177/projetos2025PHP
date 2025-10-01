9cc03db2d80161c0456ef947efe83ab8fc2588c1
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-30 22:12:48 -0300

    wilço

---
+++ EXERCICIOS 30.09/ex5.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nome =['wilson','Salve', 'Juremo', 'Pastel Do Võ zé', 'Pc da china', 'Luuqeta', 'Jamelão vai pro canto', 'gostaria de jantar um hamburguer', 'Chat gpt de souza', 
+'Gemini Teofilo neves'];
+$idades = [23,22,21,15,18,16,15,14,18,22,];
+$cidade = ['São paulo', 'Santos', 'Osasco', 'São caetano do sul', 'Ribeirão Preto','São paulo', 'Santos', 'Osasco', 'São caetano do sul', 'Ribeirão Preto',];
+
+    echo "idade e nome de todos usuários cadastrados";
+
+for ($cont = 0; $cont<10; $cont++){
+echo "Nome: ". $nome[$cont];  $idades[$cont];
+}
+
+
+?>
