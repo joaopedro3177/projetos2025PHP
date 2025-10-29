77dc2d32c3d71802b9870fc57d8d3bdcfa84adaf
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-10-28 21:30:16 -0300

    Sessão

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-		<!DOCTYPE html>
+<!-- 		<!DOCTYPE html>
 		<html lang="pt-br">
 		<head>
 			<meta charset="utf-8">
@@ -115,4 +115,4 @@
 
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 		</body>
-		</html>
+		</html> -->
