3ed8776fa4df2bb1419f2c4a1189599fdfb610be
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-11-18 22:20:10 -0300

    atividade finalizada

--- cookies/atvdCookies/formulario.php
+++ cookies/atvdCookies/formulario.php
@@ -1,28 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Formulário</title>
-</head>
-<body>
-	<h2>Votação</h2>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Formulário</title>
 
-	<form action="retorno.php" method="post">
-		<label>Vote no maior time</label><br>
+	</head>
+	<body>
+		<h2>Votação</h2>
 
-		<label>São Paulo</label>
-		<input type="radio" name="voto" value="saopaulo"><br>
-		<label>Santos</label>
-		<input type="radio" name="voto" value="santos"><br>
-		<label>Corinthians</label>
-		<input type="radio" name="voto" value="gamba"><br>
-		<label>Palmeiras</label>
-		<input type="radio" name="voto" value="palmeiras"><br>
+		<form action="retorno.php" method="post">
+
+		<!-- <div class="container"> -->
+			<label>Vote no maior time</label><br>
+			<div class="containerr">
+				<label>São Paulo</label>
+				<input type="radio" name="voto" value="saopaulo"> 
+				<label>Santos</label>
+				<input type="radio" name="voto" value="santos">
+				<label>Corinthians</label>
+				<input type="radio" name="voto" value="gamba">
+				<label>Palmeiras</label>
+				<input type="radio" name="voto" value="palmeiras"><br>
+			</div>
+			<div class="image">
 
 
-		<button type="submit">Enviar</button>
+			<img src="Saopaulo.png">
+			<img src="santos.png">
+			<img src="Corinthians.png">
+			<img src="palmeiras.jpg">
+			</div>
+			
+		<!-- </div> -->
 
-	</form>
-</body>
-</html>
+			<button type="submit">Enviar</button>
+
+		</form>
+	</body>
+	</html>
